45c01dba2cc40a34aa1945800016898de0dec853
DONTKILLME <ashishnegi0511@gmail.com>
2024-11-01 18:51:36 +0530

    Fixed Rigid Body (Ground Added)

--- src/App/World/Physics.js
+++ src/App/World/Physics.js
@@ -14,14 +14,30 @@ export default class Physics {
       const geomentry = new THREE.BoxGeometry(1, 1, 1);
       const material = new THREE.MeshStandardMaterial({ color: "orange" });
       this.cubeMesh = new THREE.Mesh(geomentry, material);
+      this.cubeMesh.position.y = 10;
+      this.cubeMesh.rotation.x = 1;
+      this.cubeMesh.rotation.z = 1;
       this.scene.add(this.cubeMesh);
+
+      const groundGeometry = new THREE.BoxGeometry(10, 1, 10);
+      const groundMaterial = new THREE.MeshStandardMaterial({ color: "red" });
+      this.groundMesh = new THREE.Mesh(groundGeometry, groundMaterial);
+      this.scene.add(this.groundMesh);
 
       // Create Physics Wolrld Object
       const rigidBodyType = RAPIER.RigidBodyDesc.dynamic();
       this.rigidBody = this.world.createRigidBody(rigidBodyType);
+      this.rigidBody.setTranslation(this.cubeMesh.position);
+      this.rigidBody.setRotation(this.cubeMesh.quaternion);
 
       const colliderType = RAPIER.ColliderDesc.cuboid(0.5, 0.5, 0.5);
       this.world.createCollider(colliderType, this.rigidBody);
+
+      const groundRigidBodyType = RAPIER.RigidBodyDesc.fixed();
+      this.groundRigidBody = this.world.createRigidBody(groundRigidBodyType);
+
+      const groundColliderType = RAPIER.ColliderDesc.cuboid(5, 0.5, 5);
+      this.world.createCollider(groundColliderType, this.groundRigidBody);
 
       this.rapierLoaded = true;
     });
